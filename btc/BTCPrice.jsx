0df4c8ad7342a607c17212b92b668d0c069d85e3
ATHAREUM OFFICIAL <athareumath@gmail.com>
2026-06-12 15:34:12 -0500

    Delete btc directory

--- btc/BTCPrice.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-// BTCPrice logic is now integrated into TokenStats.jsx
-export default function BTCPrice() { return null; }
--- btc/MyBalance.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-// MyBalance logic is now integrated into TokenStats.jsx
-export default function MyBalance() { return null; }

bbf7f834d75084910135b8d03a8e4455ff289f9e
ATHAREUM OFFICIAL <athareumath@gmail.com>
2026-06-12 15:41:56 -0500

    Add files via upload

---
+++ app/src/components/BTCPrice.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+// BTCPrice logic is now integrated into TokenStats.jsx
+export default function BTCPrice() { return null; }
---
+++ app/src/components/MyBalance.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+// MyBalance logic is now integrated into TokenStats.jsx
+export default function MyBalance() { return null; }
